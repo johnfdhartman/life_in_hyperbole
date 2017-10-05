a08bddce200f42cb193a75f97164602c376813f5
John Hartman <john.f.d.hartman@gmail.com>
2017-10-05 16:46:39 -0700

    cells now store the ids of their neighbors

--- javascript/cell.js
+++ javascript/cell.js
@@ -9,37 +9,37 @@ class Cell {
   }
 
   addParent(parent) {
-    this.parents.push(parent);
+    this.parents.push(parent.id);
     parent.addChild(this);
   }
 
   addChild(child) {
-    this.children.push(child);
+    this.children.push(child.id);
     // child.addParent(this); this causes a loop
   }
 
   addRightSibling(sibling) {
-    this.siblings.push(sibling);
-    sibling.addLeftSibling(sibling);
+    this.siblings.push(sibling.id);
+    sibling.addLeftSibling(sibling.id);
   }
 
   addLeftSibling(sibling) {
-    this.siblings.unshift(sibling);
+    this.siblings.unshift(sibling.id);
   }
 
   neighbors() {
     return (this.parents).concat(this.siblings).concat(this.children);
   }
 
-  findNextState(rule) {
-    //rule is a callback that takes a number of on cells and returns true or false
-    //true if the cell should turn on, false if it should turn off
-    let numNeighborsOn = 0;
-    this.neighbors().forEach((neighbor) => {
-      numNeighborsOn += (neighbor.state ? 1 : 0);
-    });
-    this.nextState = rule(numNeighborsOn);
-  }
+  // findNextState(rule) {
+  //   //rule is a callback that takes a number of on cells and returns true or false
+  //   //true if the cell should turn on, false if it should turn off
+  //   let numNeighborsOn = 0;
+  //   this.neighbors().forEach((neighbor) => {
+  //     numNeighborsOn += (neighbor.state ? 1 : 0);
+  //   });
+  //   this.nextState = rule(numNeighborsOn);
+  // }
 
   updateState() {
     this.state = this.nextState;
